a8e71eec389a855a2b2ae71b6e4fd3874444abfe
esthejas <esthejas@gmail.com>
2023-06-01 13:26:05 +0530

    completed ui part and api

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongooose = require("mongoose");
+const dotenv = require("dotenv")
+const userRoute = require("./routes/user");
+const cartRoute = require("./routes/Cart");
+const orderRoute = require("./routes/Order");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/Product");
+
+dotenv.config();
+
+mongooose
+.connect(process.env.MONGO_URL)
+ .then(() => console.log("Database Connected!!!"))
+ .catch((err) => {
+    console.log(err);
+ });
+
+app.use(express.json());
+app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
+app.use("/api/orders",orderRoute);
+app.use("/api/carts",cartRoute);
+app.use("/api/products",productRoute);
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("server started!");
+})
---
+++ client1/src/App.js
@@ -0,0 +1,14 @@
+// import Home from "./pages/Home";
+// import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import ProductList from "./pages/ProductList";
+
+function App() {
+  return (
+    <Cart/>
+  );
+}
+
+export default App;
